37da85dbcdf3822699a2d7597c7795e604a25a1b
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2020-09-10 17:24:36 +0200

    Fix typo. Fix formatting. Remove unused scanner package.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,20 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
             } else if (i == 42) {
-                System.out.println("Answear to the ultimate question of life, the universe, and everything");
+                System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");
             } else if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
                 System.out.println("Buzz");
-            }  else {
+            } else {
                 System.out.println(i);
             }
-
-
         }
     }
 }
