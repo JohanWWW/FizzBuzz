c7256a6668be1a7f1be036a8df5c9162aba51fa5
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2020-09-10 17:43:02 +0200

    Minor correction

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
+                System.out.println("Fizzbuzz");
             } else if (i == 42) {
                 System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");
             } else if (i % 3 == 0) {
